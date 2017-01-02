e90389fd3b1291a9ae952465ab186d3a818d360a
DIEPNGUYEN\diepn <diepnguyen1905@outlook.com>
2017-01-02 16:07:17 +0700

    Stuctural directives *ngIf *ngFor *ngSwitch

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -4,13 +4,22 @@ import {Component} from '@angular/core';
     selector:'my-tutorial',
     template:`
     <h2>{{ title }}</h2>
-    <input type="text" [(ngModel)]="fName" />
-    <input type="text" [(ngModel)]="lName"/>
-    <br>
-    <p> FullName : {{ fName }} {{ lName }}</p>
+    <h3 *ngIf="showLineIf"> This is ngIf Directive </h3>
+    <div [ngSwitch]="color">
+        <div *ngSwitchCase="'red'">this is line red</div>
+        <div *ngSwitchCase="'blue'">this is line blue</div>
+        <div *ngSwitchCase="'green'">this is line green</div>
+        <div *ngSwitchCase="'black'">this is line black</div>
+    </div>
+    <ul>
+    <li *ngFor="let color of colors ">{{ color }}</li>
+    </ul>
     `
 })
 export class TutorialComponent{
     public title = " Hello Angular Js 2 ";
+    public showLineIf = false;
+    public color = 'red';
+    public colors: string[] =  ["red" , "green","blue"];
 
 }
