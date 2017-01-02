dda24559c95df39ea1853d5c0bc6307b5c575177
DIEPNGUYEN\diepn <diepnguyen1905@outlook.com>
2017-01-02 10:57:36 +0700

    using styles component

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -2,7 +2,11 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    template: '<h1>First Component</h1>'
+    template: '<h1>First Component</h1>' +
+    '<h2>Second Component</h2>'
+    ,
+    // sử dựng sytles cho từng component
+    styles:['h1{color: red }' , 'h2{ color: blue}']
 })
 
 export class TutorialComponent {
