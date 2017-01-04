117bbfa0b74c3689fd4a849a07f5199b3d9eec62
DIEPNGUYEN\diepn <diepnguyen1905@outlook.com>
2017-01-05 00:43:51 +0700

    Attribute Driectives( ngClass and ngStyles )

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -1,25 +1,34 @@
 import {Component} from '@angular/core';
 // binding class or syte cho element
 @Component({
-    selector:'my-tutorial',
-    template:`
+    selector: 'my-tutorial',
+    template: `
     <h2>{{ title }}</h2>
-    <h3 *ngIf="showLineIf"> This is ngIf Directive </h3>
-    <div [ngSwitch]="color">
-        <div *ngSwitchCase="'red'">this is line red</div>
-        <div *ngSwitchCase="'blue'">this is line blue</div>
-        <div *ngSwitchCase="'green'">this is line green</div>
-        <div *ngSwitchCase="'black'">this is line black</div>
-    </div>
-    <ul>
-    <li *ngFor="let color of colors ">{{ color }}</li>
-    </ul>
-    `
+    <p [ngClass]="{classOne: cone , classTwo: ctwo } ">This is ng Class</p>
+    <button (click)="toggle()">Toggle</button>
+    
+  <p [ngStyle]="{color: style ,background: style2 }">This is paragaph will be apply to ngStyle </p>
+    `,
+    styles: [
+        `
+            .classOne {
+           color: red;
+           }
+           .classTwo {
+           color: blue;
+           }
+        `
+    ]
 })
-export class TutorialComponent{
+export class TutorialComponent {
     public title = " Hello Angular Js 2 ";
-    public showLineIf = false;
-    public color = 'red';
-    public colors: string[] =  ["red" , "green","blue"];
+    public cone: boolean = true;
+    public ctwo:boolean = false;
+    toggle(){
+        this.cone = !this.cone;
+        this.ctwo = !this.ctwo;
+    }
 
+    public style2 = 'blue';
+    public style = 'red';
 }
